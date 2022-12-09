76df33974bdc821c3adcafa47f0db49c70e94946
irmnrmhs <irmnrmhs@gmail.com>
2022-12-10 06:21:29 +0700

    CRUD API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,15 @@ use App\Http\Controllers\API\AuthController;
 // });
 
 Route::middleware('auth:sanctum')->group(function () {
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/books', [BookController::class, 'books']);
+    Route::post('/book/create', [BookController::class, 'create']);
+    Route::post('/book/update/{id}', [BookController::class, 'update']);
+    Route::post('/book/delete/{id}', [BookController::class, 'delete']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
